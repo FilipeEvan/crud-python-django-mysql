adb52b54d6955675308d8f4a002ed955374a06af
FilipeEvan <filipeevan@outlook.com>
2022-05-09 22:02:17 -0300

    date format commit

--- AGContatos/settings.py
+++ AGContatos/settings.py
@@ -119,6 +119,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = False
+
+DATE_FORMAT = 'd/m/Y'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
